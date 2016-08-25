2db7baa4bfd8912bff6d52b9c904ef8d278b89c0
aifos710 <sofia.m.caballero@gmail.com>
2016-08-25 14:59:53 -0500

    promedio

---
+++ app.js
@@ -0,0 +1,5 @@
+var num1 = prompt("Ingrese primer número");
+var num2 = prompt("Ingrese segundo número");
+var num3 = prompt("Ingrese tercer número");
+var respuesta = (parseInt(num1)+parseInt(num2)+parseInt(num3)) / 3;
+document.write(respuesta);
